dc90aa941bced3e351d76545cbe958cfb91e4af6
sandilokaa <sandyloka790@gmail.com>
2024-11-04 00:52:12 +0700

    docs: add Required

--- user/src/modules/dto/update-profile.dto.ts
+++ user/src/modules/dto/update-profile.dto.ts
@@ -4,46 +4,51 @@ import { IsString, IsOptional } from 'class-validator';
 export class UpdateProfileDto {
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   name?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   gender?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   birthday?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   horoscope?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   zodiac?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   height?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string' })
+  @ApiProperty({ type: 'string', required: false, nullable: true })
   weight?: string;
 
   @IsOptional()
   @IsString()
-  @ApiProperty({ type: 'string', format: 'binary' })
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    required: false,
+    nullable: true,
+  })
   image?: Express.Multer.File;
 
   @IsOptional()
   @IsString({ each: true })
-  @ApiProperty({ type: [String] })
+  @ApiProperty({ type: [String], required: false, nullable: true })
   interest?: string[];
 }
